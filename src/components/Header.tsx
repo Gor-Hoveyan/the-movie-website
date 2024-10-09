1848a3b90054ca63810f186e899effaf68b70389
Gor-Hoveyan <gorpro2004@mail.ru>
2024-10-09 04:36:07 +0400

    Add header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,12 +5,26 @@ export default function Header() {
   return (
     <header className="w-4/5 mx-auto pt-2">
       <div className="flex flex-row justify-between">
-        <Link
-          href={`/home`}
-          className="text-gray-500 font-extrabold text-3xl font-mono"
-        >
-          TMW
-        </Link>
+        <div className="flex items-center">
+          <Link
+            href={`/home`}
+            className="text-gray-600 font-extrabold text-3xl font-mono"
+          >
+            TMW
+          </Link>
+          <Link
+            href={`/shows`}
+            className="px-20 font-extrabold text-gray-500  text-lg"
+          >
+            Movies
+          </Link>
+          <Link
+            href={`/movies`}
+            className="font-extrabold text-gray-500  text-lg"
+          >
+            Shows
+          </Link>
+        </div>
         <Search />
       </div>
     </header>
